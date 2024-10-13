b1226b141bed01529f0b500df0d57e5861d063f2
Andreas Turesson <andreas.turesson25@gmail.com>
2024-10-13 15:23:31 +0200

    Add basic support for adding new attacks to player and executing existing attacks.

---
+++ Assets/Scripts/Support/Cleanup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.PlayerScripts.Support
+{
+    public class Cleanup : MonoBehaviour
+    {
+        public float Delay = 0.1f;
+
+        void Update()
+        {
+            Destroy(gameObject, Delay);
+        }
+    }
+}
